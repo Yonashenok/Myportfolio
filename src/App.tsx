6d1cad9f13a8ec8a3f5fe61047d9ac009ae0ad4c
Yonashenok <yonashenok24@gmail.com>
2023-11-28 19:43:09 +0300

    feat: add footer on layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/home/Home';
+import About from './components/about/About';
+import Project from './components/project/Project';
+import Contact from './components/contact/Contact';
 
 export default function App(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
       </Route>
     </Routes>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Header from './header/Header';
+import { Footer } from './footer/Footer';
 
 const Layout = (): JSX.Element => {
   return (
-    <main className="bg-[#18181b] w-[100vw] md:w-[70vw] h-[140vh]">
-      <Header />
-      <Outlet />
+    <main className="bg-[#18181b] w-[100svw] md:w-[60svw]  md:h-[170svh] flex flex-col justify-between gap-0">
+      <div>
+        <Header />
+        <Outlet />
+      </div>
+      <Footer />
     </main>
   );
 };
